7f7082c779c0fd6c30c6eddbf9a1253840d1cfa6
Carlos Julio Marin Lopez <carlos.marin@carroya.com>
2021-10-08 17:14:55 -0500

    commit initical

---
+++ spring-boot-docker/src/main/java/com/prueba/SpringBootDockerApplication.java
@@ -0,0 +1,14 @@
+package com.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class SpringBootDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+}
---
+++ spring-boot-docker/src/main/java/com/prueba/controller/PruebaRest.java
@@ -0,0 +1,16 @@
+package com.prueba.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PruebaRest {
+
+	
+	@GetMapping("/")
+	public String respuesta() {
+		return "Hello from rest controller !!";
+	}
+}
